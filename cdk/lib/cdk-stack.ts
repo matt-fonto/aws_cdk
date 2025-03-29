00f5e1f3d36e63e71663637596e2344881c8d8fc
matt-fonto <mateus.fonto@gmail.com>
2025-03-29 11:51:37 +0100

    feat: creation of l1 and l2 constructs

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from "aws-cdk-lib";
+import { Bucket, CfnBucket } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,18 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // L1 construct (low-level)
+    const level1S3Bucket = new CfnBucket(this, "MyFirstLevel1ConstructBucket", {
+      versioningConfiguration: {
+        status: "Enabled",
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // L2 construct (better)
+    const level2S3Bucket = new Bucket(this, "MyFirstLevel2ConstructBucket", {
+      bucketName: "mstrappazzonl2bucket", // this name needs to be unique
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // on the l2, it's set to RETAIN by default. Set to destroy to remove the previous bucket. Use it carefully
+      versioned: true,
+    });
   }
 }
