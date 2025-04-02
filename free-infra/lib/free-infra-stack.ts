57cb74e1daca3c535d1d55c748f9a14240d79c93
matt-fonto <mateus.fonto@gmail.com>
2025-04-02 20:42:40 +0200

    lib/free-infra-stack

--- free-infra/lib/free-infra-stack.ts
+++ free-infra/lib/free-infra-stack.ts
@@ -1,16 +1,59 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as cognito from "aws-cdk-lib/aws-cognito";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 
 export class FreeInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const userPool = new cognito.UserPool(this, "UserPool", {
+      selfSignUpEnabled: true,
+      signInAliases: { email: true },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FreeInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const table = new dynamodb.Table(this, "AppTable", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    const backend = new lambda.Function(this, "BackendFn", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset("../backend-functions"),
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    table.grantReadWriteData(backend);
+
+    const api = new apigateway.LambdaRestApi(this, "APIGateway", {
+      handler: backend,
+    });
+
+    const siteBucket = new s3.Bucket(this, "SiteBucket", {
+      websiteIndexDocument: "index.html",
+      publicReadAccess: false,
+    });
+
+    new s3deploy.BucketDeployment(this, "DeployFrontend", {
+      sources: [s3deploy.Source.asset("../amplify-frontend/dist")],
+      destinationBucket: siteBucket,
+    });
+
+    const distribution = new cloudfront.Distribution(this, "CDN", {
+      defaultBehavior: {
+        // @ts-ignore
+        origin: new origins.S3BucketOrigin(siteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+    });
   }
 }
